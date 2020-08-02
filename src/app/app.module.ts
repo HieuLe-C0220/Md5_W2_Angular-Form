5e000ee719d6f1b7f37a466b6a358a8e000b55a9
Hieu.Le <letrunghieu2602@gmail.com>
2020-08-02 23:40:19 +0700

    hoàn thành todo và đăng ký

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { RegisterFinalComponent } from './register-final/register-final.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    RegisterFinalComponent,
+    RegisterComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
